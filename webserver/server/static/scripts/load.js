1c085993006b455df433a816c3b2293f26998d25
Harry Reeder <harry@harryreeder.co.uk>
2017-01-20 23:12:36 +0000

    Add a demo template

--- webserver/server/static/scripts/load.js
+++ webserver/server/static/scripts/load.js
@@ -1,6 +1,20 @@
 /**
  * Created by szczocik on 20/01/17.
  */
+function getTemplate(templateName, callback) {
+    $.ajax({
+        url: "/static/templates/" + templateName + ".hbs",
+        cache: false,
+        success: callback
+    });
+}
+
 $(function(){
-    
-})
+    getTemplate("test", function(tpl_source) {
+        var tpl = Handlebars.compile(tpl_source);
+        $('#container').html(tpl({
+            title: "Test",
+            body: "testtesttest"
+        }))
+    })
+});

1d47aeac5c2cc3118bd729a657fddb3db5afb34c
Harry Reeder <harry@harryreeder.co.uk>
2017-01-21 11:19:18 +0000

    Refactor level loading

--- webserver/server/static/scripts/load.js
+++ webserver/server/static/scripts/load.js
@@ -24,46 +24,12 @@ $(function(){
         var level = data.level;
         getTemplate(level, function(tpl_source) {
             var tpl = Handlebars.compile(tpl_source);
-            $('#main-area').html(tpl({
-                title: "Test",
-                body: "testtesttest"
-            }))
-            create_wave();
-            $('#waves').css({'border':'2px black solid'});
+            $('#main-area').html(tpl());
+            if (level === "entry") {
+                loadEntryPuzzle();
+            }
         });
     });
 });
 
 
-function create_wave() {
-    var waves = new SineWaves({
-        el: document.getElementById('waves'),
-        speed: 2,
-        width: function() {
-            return $('#main-area').width();
-        },
-        height: function() {
-            return $(window).height()*0.7;
-        },
-        wavesWidth: '105%',
-        ease: 'Linear',
-        waves: [
-            {
-                timeModifier: 1,
-                lineWidth: 3,
-                amplitude: 150,
-                wavelength: 120,
-                segmentLength: 20,
-                //       strokeStyle: 'rgba(255, 255, 255, 0.5)'
-            },
-            {
-                timeModifier: 1,
-                lineWidth: 2,
-                amplitude: 170,
-                wavelength: 80,
-                strokeStyle: 'rgba(255, 0, 0, 1)'
-            },
-        ]
-    });
-
-}
---
+++ webserver/server/static/scripts/puzzles/entry.js
@@ -0,0 +1,36 @@
+function loadEntryPuzzle() {
+    create_wave();
+    $('#waves').css({'border':'2px black solid'});
+}
+
+function create_wave() {
+    var waves = new SineWaves({
+        el: document.getElementById('waves'),
+        speed: 2,
+        width: function() {
+            return $('#main-area').width();
+        },
+        height: function() {
+            return $(window).height()*0.7;
+        },
+        wavesWidth: '105%',
+        ease: 'Linear',
+        waves: [
+            {
+                timeModifier: 1,
+                lineWidth: 3,
+                amplitude: 150,
+                wavelength: 120,
+                segmentLength: 20,
+                //       strokeStyle: 'rgba(255, 255, 255, 0.5)'
+            },
+            {
+                timeModifier: 1,
+                lineWidth: 2,
+                amplitude: 170,
+                wavelength: 80,
+                strokeStyle: 'rgba(255, 0, 0, 1)'
+            },
+        ]
+    });
+}

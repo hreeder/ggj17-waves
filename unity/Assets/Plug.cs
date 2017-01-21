d8ec53d17610bea243b205e38ebebeffb1c0c44b
Grant Smith <migiesmith@gmail.com>
2017-01-21 12:03:04 +0000

    Updated Plug

--- unity/Assets/Plug.cs
+++ unity/Assets/Plug.cs
@@ -3,34 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Plug : MonoBehaviour {
-
-	private GameObject attatched;
-
+    
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-    public void unplug()
-    {
-        Rigidbody r = attatched.GetComponent<Rigidbody>();
-        r.AddForce(2, 2, 2);
-        attatched = null;
-    }
 
 	// Update is called once per frame
 	void Update () {
-        if(attatched != null){
-			attatched.transform.position = this.transform.position;
-		}
+
     }
 
 	void OnTriggerEnter(Collider other) {
         if (other.CompareTag("pluggable"))
         {
-            attatched = other.gameObject;
+            HingeJoint hinge = other.gameObject.AddComponent<HingeJoint>();
+            hinge.breakForce = 10;
         }
-        //Destroy(other.gameObject);
     }
 
 }

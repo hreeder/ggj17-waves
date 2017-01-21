f427ec24f88f2513143d6368ffd6984450246f1b
Harry Reeder <harry@harryreeder.co.uk>
2017-01-21 11:13:19 +0000

    Merge

--- webserver/server/static/scripts/websockets.js
+++ webserver/server/static/scripts/websockets.js
@@ -1,4 +1,5 @@
 var callbacks = {};
+var actionCallbacks = {};
 
 function startWebsocket() {
     window.websocket = new WebSocket("ws://localhost:8888/ws/client");
@@ -7,27 +8,30 @@ function startWebsocket() {
         var data = JSON.parse(msg.data);
         console.log(data);
 
-        var chain = callbacks[data.event];
-        if(typeof chain == 'undefined') return; // no callbacks for this event
-        for(var i = 0; i < chain.length; i++){
-          chain[i](data);
+        if (data.event === "action") {
+            var chain = actionCallbacks[data.level];
+            if(typeof chain == 'undefined') return; // no callbacks for this event
+            for(var i = 0; i < chain.length; i++){
+              chain[i](data);
+            }
+        } else {
+            var chain = callbacks[data.event];
+            if(typeof chain == 'undefined') return; // no callbacks for this event
+            for(var i = 0; i < chain.length; i++){
+              chain[i](data);
+            }
         }
     };
-
-    // Tell the server this is client 1 (swap for client 2 of course)
-    //    websocket.send(JSON.stringify({
-    //      id: "client1"
-    //    }));
-    //
-    //    // Tell the server we want to send something to the other client
-    //    websocket.send(JSON.stringify({
-    //      to: "client2",
-    //      data: "foo"
-    //    }));
 }
 
 function addWebsocketCallback(event, callback) {
     callbacks[event] = callbacks[event] || [];
     callbacks[event].push(callback);
+    return this;// chainable
+}
+
+function addActionCallback(level, callback) {
+    actionCallbacks[event] = actionCallbacks[event] || [];
+    actionCallbacks[event].push(callback);
     return this;// chainable
 }

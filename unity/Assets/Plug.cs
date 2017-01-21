67f2cfb3bcceba8a5671ad26c7acf1484962d22d
dink_dunk <duncanduguuid@gmail.com>
2017-01-21 00:11:10 +0000

    N1 update

---
+++ unity/Assets/Plug.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plug : MonoBehaviour {
+
+	private GameObject attatched;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(attatched != null){
+			attatched.transform.position = this.transform.position;
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		attatched = other.gameObject;
+        //Destroy(other.gameObject);
+    }
+
+}

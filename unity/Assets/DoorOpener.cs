0a8d20d94e4a39022e51d03afb988e3ae5a74d7b
Grant Smith <migiesmith@gmail.com>
2017-01-21 18:59:17 +0000

    Door Opener

---
+++ unity/Assets/DoorOpener.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour {
+
+    public Transform door0;
+    private Vector3 door0Start;
+
+    public Transform door1;
+    private Vector3 door1Start;
+
+    private bool isOpen = false;
+    private bool moving = false;
+    private float timeDelta = 0.0f;
+
+
+    private NewtonVR.NVRButton inputInfo;
+
+    // Use this for initialization
+    void Start () {
+        door0Start = door0.position;
+        door1Start = door1.position;
+
+        inputInfo = this.GetComponent<NewtonVR.NVRButton>();
+    }
+
+    public void toggle()
+    {
+        if (isOpen)
+        {
+            close();
+        }
+        else
+        {
+            open();
+        }
+    }
+
+    void open()
+    {
+        moving = true;
+        isOpen = true;
+        timeDelta = 0.0f;
+    }
+	
+    void close()
+    {
+        isOpen = false;
+        moving = true;
+        timeDelta = 1.0f;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (moving)
+        {
+            if (isOpen)
+            {
+                door0.position = door0Start - door0.right * (Mathf.Sin(timeDelta) * 0.3f + 0.3f);
+                door1.position = door1Start - door1.right * (Mathf.Sin(timeDelta) * 0.3f + 0.3f);
+                timeDelta += Time.deltaTime;
+
+                if (timeDelta >= 1.0f)
+                    moving = false;
+            }
+            else
+            {
+                door0.position = door0Start - door0.right * (Mathf.Sin(timeDelta) * 0.3f + 0.3f);
+                door1.position = door1Start - door1.right * (Mathf.Sin(timeDelta) * 0.3f + 0.3f);
+                timeDelta -= Time.deltaTime;
+
+                if (timeDelta <= 0.0f)
+                    moving = false;
+            }
+        }
+        if (inputInfo.ButtonDown)
+        {
+            toggle();
+        }
+    }
+}

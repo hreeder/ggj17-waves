7cfd7bf2bc92e803c8aede5ccf7a5e5ab1afef89
dink_dunk <duncanduguuid@gmail.com>
2017-01-22 13:43:21 +0000

    scene shit

---
+++ unity/Assets/LineBetween.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineBetween : MonoBehaviour {
+
+	public Transform start, end;
+	private LineRenderer line;
+
+
+	// Use this for initialization
+	void Start () {
+		line = gameObject.GetComponent<LineRenderer>();
+		line.SetPosition(0, start.position);
+		line.SetPosition(1, end.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		line.SetPosition(0, start.position);
+		line.SetPosition(1, end.position);
+		
+	}
+}
--- unity/Assets/MapUpdater.cs
+++ unity/Assets/MapUpdater.cs
@@ -4,22 +4,16 @@ using UnityEngine;
 
 public class MapUpdater : MonoBehaviour {
 
-    public Networker networker;
+    private Networker networker;
     public GameObject gman0;
     public GameObject gman1;
 
     private int step = 0;
 
+    bool hasStarted = false;
+
 	// Use this for initialization
 	void Start () {
-        LoadMapLevelObject evt_loadMap = new LoadMapLevelObject();
-        evt_loadMap._event = "load-level";
-        evt_loadMap.level = "puzzle-map";
-        evt_loadMap.x1 = (int)(gman0.transform.position.x / 6);
-        evt_loadMap.y1 = (int)(gman0.transform.position.z / 6);
-        evt_loadMap.x2 = (int)(gman1.transform.position.x / 6);
-        evt_loadMap.y2 = (int)(gman1.transform.position.z / 6);
-        networker.ws.SendString(evt_loadMap.getJSON());
     }
 
     public void onConnected(Networker networker)
@@ -29,6 +23,19 @@ public class MapUpdater : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        if(networker != null && hasStarted){
+            LoadMapLevelObject evt_loadMap = new LoadMapLevelObject();
+            evt_loadMap._event = "load-level";
+            evt_loadMap.level = "puzzle-map";
+            evt_loadMap.x1 = (int)(gman0.transform.position.x / 6);
+            evt_loadMap.y1 = (int)(gman0.transform.position.z / 6);
+            evt_loadMap.x2 = (int)(gman1.transform.position.x / 6);
+            evt_loadMap.y2 = (int)(gman1.transform.position.z / 6);
+            networker.ws.SendString(evt_loadMap.getJSON());
+
+            hasStarted = true;
+        }
+
         if(step % 10 == 0) {
             LoadMapLevelObject evt_loadMap = new LoadMapLevelObject();
             evt_loadMap.x1 = (int)(gman0.transform.position.x / 6);

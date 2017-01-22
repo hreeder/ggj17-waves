6d86be9fe9be4ffa7b09a3cef206ef7bbe95ae25
Marcin Szczot <mszczot@gmail.com>
2017-01-22 12:02:21 +0000

    table grid

--- webserver/server/static/scripts/puzzles/map.js
+++ webserver/server/static/scripts/puzzles/map.js
@@ -11,6 +11,7 @@ function loadMapPuzzle(msg) {
 
 function load_map(msg){
     create_table();
+    // create_canvas();
     manipulateMap(msg)
 }
 
@@ -19,10 +20,10 @@ function load_map(msg){
 function create_table() {
 
     var table_str = "<table id='table_grid'>";
-    for (var i = 14; i >= 0; i--){
+    for (var i = 5; i >= 0; i--){
         table_str += "<tr>";
 
-        for (var j = 0; j < 15; j++) {
+        for (var j = 0; j < 6; j++) {
             table_str += "<td><div id='" + j+"-"+i+ "'></div></div></td>";
         }
         table_str += "</tr>";
@@ -30,10 +31,31 @@ function create_table() {
     table_str += "</table>";
     $('#full_map').append(table_str);
     $('#full_map').show();
+    $('#table_grid td').click(function(){
+        var coordinates = $(this).find('div').attr('id').split('-');
+        console.log("x: " + coordinates[0] + " y: " + coordinates[1]);
+    })
 }
 
 function manipulateMap(msg){
     //$("td div").css('background','none');
-    $("#"+msg.x1+"-"+msg.y1).css('background','red');
-    $("#"+msg.x2+"-"+msg.y2).css('background','red');
+    $("#"+msg.x1+"-"+msg.y1).addClass('enemy');
+    $("#"+msg.x2+"-"+msg.y2).addClass('enemy');
+}
+
+function create_canvas() {
+    var newCanvas =
+        $('<canvas/>',{'id':'map_canvas'})
+            .width(800)
+            .height(800);
+
+    $('#full_map').append(newCanvas);
+
+    var canvas = $('#map_canvas')[0];
+    var ctx = canvas.getContext("2d");
+    var img = new Image;
+    img.onload = function() {
+        ctx.drawImage(this, 10,10,0,0,0,0,800,800);
+    };
+    img.src = "/static/images/map.png";
 }

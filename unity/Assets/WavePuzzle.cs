4a66c6c7016f6647a8d3261a4e0a1753aa69264c
dink_dunk <duncanduguuid@gmail.com>
2017-01-22 00:12:04 +0000

    Improved Networking
    Sliders also stop moving after completing the challenge

--- unity/Assets/WavePuzzle.cs
+++ unity/Assets/WavePuzzle.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class WavePuzzle : MonoBehaviour {
 
-    public Networker networker;
 
     public float amplitude = 0.0f;
     public float frequency = 0.0f;
     public float phase = 0.0f;
 
-    public GameObject particles;
+    private bool isComplete = false;
+
+    private Networker networker;
+    
 
     private enum SLIDER
     {
@@ -39,40 +42,54 @@ public class WavePuzzle : MonoBehaviour {
     private NewtonVR.NVRSlider freSlider;
     private NewtonVR.NVRSlider phaSlider;
 
-    private float correctAmplitude = 5.0f;
-    private float correctFrequency = 5.0f;
-    private float correctPhase = 5.0f;
+    private float correctAmplitude = 1.0f;
+    private float correctFrequency = 1.0f;
+    private float correctPhase = 4.0f;
+
+    private bool startedLevel;
 
     // Use this for initialization
     void Start () {
+        startedLevel = false;
+
         ampSlider = this.transform.FindChild("Amplitude").FindChild("Slider").GetComponent<NewtonVR.NVRSlider>();
         freSlider = this.transform.FindChild("Frequency").FindChild("Slider").GetComponent<NewtonVR.NVRSlider>();
         phaSlider = this.transform.FindChild("Phase").FindChild("Slider").GetComponent<NewtonVR.NVRSlider>();
 
-        sliderInfo[0] = new SliderInfo(false, 0.0f, 6.0f); // Amplitude
-        sliderInfo[1] = new SliderInfo(false, 0.0f, 7.0f); // Frequency
-        sliderInfo[2] = new SliderInfo(false, 0.0f, 360.0f); // Phase
+        sliderInfo[0] = new SliderInfo(false, 1.0f, 4.0f); // Amplitude
+        sliderInfo[1] = new SliderInfo(false, 10.0f, 100.0f); // Frequency
+        sliderInfo[2] = new SliderInfo(false, 1.0f, 4.0f); // Phase
 
-        networker = this.gameObject.transform.Find("Networker").GetComponent<Networker>();
+        correctAmplitude = Random.Range(sliderInfo[0].min * 1.1f, sliderInfo[0].max * 0.9f);
+        correctFrequency = Random.Range(sliderInfo[1].min * 1.1f, sliderInfo[1].max * 0.9f);
+        correctPhase = Random.Range(sliderInfo[2].min * 1.1f, sliderInfo[2].max * 0.9f);
+
+        // networker = this.gameObject.transform.Find("Networker").GetComponent<Networker>();
 
         for (int i = 0; i < sliderInfo.Length; i++)
         {
             sliderInfo[i].flipped = Random.Range(0, 100) < 50;
         }
 
+    }
+
+    public void networkerConnected(Networker networker){
+        this.networker = networker;
+
         WaveformActionObject evt_pluggedin = new WaveformActionObject();
         evt_pluggedin._event = "load-level";
         evt_pluggedin.level = "puzzle-entry";
-        evt_pluggedin.amplitude = 5.0f;
-        evt_pluggedin.frequency = 5.0f;
-        evt_pluggedin.phase = 5.0f;
+        evt_pluggedin.amplitude = this.correctAmplitude;
+        evt_pluggedin.frequency = this.correctFrequency;
+        evt_pluggedin.phase = this.correctFrequency;
         networker.ws.SendString(evt_pluggedin.getJSON());
 
     }
 
     public void tellHacker()
     {
-        particles.SetActive(true);
+        if(networker == null || isComplete)
+            return;
 
         WaveformActionObject evt_update = new WaveformActionObject();
         evt_update.level = "puzzle-entry-wave";
@@ -80,6 +97,8 @@ public class WavePuzzle : MonoBehaviour {
         evt_update.frequency = this.frequency;
         evt_update.phase = this.phase;
         networker.ws.SendString(evt_update.getJSON());
+
+
 
         if (Mathf.Abs(this.amplitude - this.correctAmplitude)/correctAmplitude < 0.1f
             && Mathf.Abs(this.frequency - this.correctFrequency) / correctFrequency < 0.1f
@@ -91,6 +110,15 @@ public class WavePuzzle : MonoBehaviour {
             evt_correct.frequency = this.correctFrequency;
             evt_correct.phase = this.correctPhase;
             networker.ws.SendString(evt_correct.getJSON());
+
+            this.isComplete = true;
+            int numChildren = this.gameObject.transform.childCount;
+            for(int i = 0; i < numChildren; i++){
+                Transform child = this.gameObject.transform.GetChild(i);
+                Transform slider = child.Find("Slider");
+                Rigidbody rb = slider.GetComponent<Rigidbody>();
+                rb.constraints = RigidbodyConstraints.FreezeAll;
+            }
         }
         
     }
@@ -115,6 +143,6 @@ public class WavePuzzle : MonoBehaviour {
         amplitude = sliderVal(SLIDER.AMPLITUDE);
         frequency = sliderVal(SLIDER.FREQUENCY);
         phase = sliderVal(SLIDER.PHASE);
-
+        tellHacker();
     }
 }
